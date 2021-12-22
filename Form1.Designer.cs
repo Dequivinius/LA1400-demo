697986615a768997c83915c9fcd802ce99b28b19
Dequivinius <80431964+Dequivinius@users.noreply.github.com>
2021-12-22 08:20:31 +0100

    Add files via upload
    Mein erster Commit 🙄

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace voluminator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bx_text2 = new System.Windows.Forms.TextBox();
+            this.lbl_value2 = new System.Windows.Forms.Label();
+            this.lbl_value = new System.Windows.Forms.Label();
+            this.bx_text1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sphere",
+            "Cube",
+            "Cylinder"});
+            this.comboBox1.Location = new System.Drawing.Point(64, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(251, 38);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(64, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "What do you want to compute the volume of?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bx_text2);
+            this.groupBox1.Controls.Add(this.lbl_value2);
+            this.groupBox1.Controls.Add(this.lbl_value);
+            this.groupBox1.Controls.Add(this.bx_text1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(404, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 328);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Values";
+            this.groupBox1.Visible = false;
+            // 
+            // bx_text2
+            // 
+            this.bx_text2.Location = new System.Drawing.Point(35, 195);
+            this.bx_text2.Name = "bx_text2";
+            this.bx_text2.Size = new System.Drawing.Size(262, 35);
+            this.bx_text2.TabIndex = 4;
+            // 
+            // lbl_value2
+            // 
+            this.lbl_value2.AutoSize = true;
+            this.lbl_value2.Location = new System.Drawing.Point(35, 149);
+            this.lbl_value2.Name = "lbl_value2";
+            this.lbl_value2.Size = new System.Drawing.Size(68, 30);
+            this.lbl_value2.TabIndex = 3;
+            this.lbl_value2.Text = "label2";
+            // 
+            // lbl_value
+            // 
+            this.lbl_value.AutoSize = true;
+            this.lbl_value.Location = new System.Drawing.Point(35, 47);
+            this.lbl_value.Name = "lbl_value";
+            this.lbl_value.Size = new System.Drawing.Size(68, 30);
+            this.lbl_value.TabIndex = 2;
+            this.lbl_value.Text = "label2";
+            // 
+            // bx_text1
+            // 
+            this.bx_text1.Location = new System.Drawing.Point(35, 90);
+            this.bx_text1.Name = "bx_text1";
+            this.bx_text1.Size = new System.Drawing.Size(252, 35);
+            this.bx_text1.TabIndex = 1;
+            this.bx_text1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Let\'s go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 236);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 139);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Compute the volume";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_value;
+        private System.Windows.Forms.TextBox bx_text1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.TextBox bx_text2;
+        private System.Windows.Forms.Label lbl_value2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace voluminator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            string selected = comboBox1.SelectedItem.ToString();
+            lbl_value2.Visible = false;
+            bx_text2.Visible = false;
+
+
+            if (selected == "Sphere")
+            {
+                show_sphere();
+            }
+
+            if (selected == "Cylinder")
+            {
+                show_cylinder();
+            }
+
+            if (selected == "Cube")
+            {
+                show_cube();
+            }
+
+        }
+
+        private void show_cube()
+        {
+            groupBox1.Visible = true;
+            pictureBox1.Visible = true;
+            lbl_value.Text = "Length";
+            pictureBox1.BackgroundImage = Image.FromFile("C:\\Users\\sazerac\\Downloads\\Cube.svg.png");
+
+        }
+
+        private void show_sphere()
+        {
+            groupBox1.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox1.BackgroundImage = Image.FromFile("C:\\Users\\sazerac\\Downloads\\Sphere.svg.png");
+
+            lbl_value.Text = "Radius";
+        }
+
+        private void show_cylinder()
+        {
+            groupBox1.Visible = true;
+            pictureBox1.Visible = true;
+            pictureBox1.BackgroundImage = Image.FromFile("C:\\Users\\sazerac\\Downloads\\Cylinder.svg.png");
+            lbl_value.Text = "Height";
+
+            lbl_value2.Visible = true;
+            lbl_value2.Text = "Radius";
+            bx_text2.Visible = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selected = comboBox1.SelectedItem.ToString();
+            double volume = -1.0;
+            double input = -1.0;
+
+            // check user input
+            if (!String.IsNullOrEmpty(bx_text1.Text)) 
+            {
+                try {
+                    input = Convert.ToDouble(bx_text1.Text);
+                } 
+                
+                catch
+                {
+                    MessageBox.Show("Enter a value");
+                    return;
+                }
+            }
+
+            switch (selected)
+            {
+                case "Cube":
+
+                    double radius = input;
+                    volume = volume_cube(radius);
+                    break;
+
+                case "Sphere":
+
+                    double sphere_radius = input;
+                    volume = volume_sphere(sphere_radius);
+                    break;
+
+                case "Cylinder":
+                    double cylinder_radius = input;
+                    double height = Convert.ToDouble(bx_text2.Text);
+                    volume = volume_cylinder(cylinder_radius, height);
+                    break;
+            }
+
+            if (volume > 0)
+            {
+                MessageBox.Show("Your volume is: \n" + volume);
+            }
+        }
+
+        public double volume_cube(double length)
+        {
+            return length * length * length;
+        }
+
+        private double volume_sphere(double radius)
+        {
+            return ( 4 / 3 ) * Math.PI * radius * radius * radius;
+        }
+
+        private double volume_cylinder(double radius, double height)
+        {
+            return radius * radius * Math.PI * height;
+        }
+    }
+}
